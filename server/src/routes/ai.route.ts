05a0d70c2dafce1e00b25ef87ff5d8621164a744
Naveenravi07 <gamerkidnav@gmail.com>
2025-07-26 10:41:08 +0530

    Added sarvam ai for audio transcripts

--- server/src/routes/ai.route.ts
+++ server/src/routes/ai.route.ts
@@ -2,6 +2,10 @@ import { Hono } from "hono";
 import { google } from "@ai-sdk/google";
 import { streamText as honoStreamText } from "hono/streaming";
 import { zValidator } from '@hono/zod-validator'
+import { sarvam } from 'sarvam-ai-sdk';
+import { experimental_transcribe as transcribe } from "ai";
+import { readFile } from "fs/promises";
+import { join } from "path";
 
 import { streamText } from "ai";
 import { aiStreamReqZodSchema } from "../zodSchemas/ai";
@@ -31,7 +35,13 @@ const aiRoutes = new Hono()
             return c.json({ error: "Failed to generate ideas" }, 500);
         }
     })
-
+    .post('/audio-transalate',async(c)=>{
+        const result = await transcribe({
+            model: sarvam.speechTranslation("saaras:v2"),
+            audio: await readFile(join(process.cwd(), "jayaram.wav")),
+        });
+        return c.text(result.text)
+    })
 
 
 export default aiRoutes;
